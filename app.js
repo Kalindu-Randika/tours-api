fd21a0de4f21765cacae3a4255ec59da1621da7f
Kalindu-Randika <kalindu.randika@gmail.com>
2023-11-21 23:22:56 +0530

    Setting up post

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
 
 
@@ -15,6 +17,23 @@ app.get('/api/v1/tours', (req, res) => {
     }
   })
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.length > 0 ? tours[tours.length - 1].id + 1 : 1;
+  const newTour = Object.assign({id: newId}, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours.json`, JSON.stringify(tours) , err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  })
+
+});
+
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message:'This is response from server', app: "natours"});
